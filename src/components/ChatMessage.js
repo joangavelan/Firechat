414acf024fbac1986f5072c123c6523df912808e
Johan Gavelan <johangavelan@gmail.com>
2021-06-01 21:27:53 -0500

    clean up

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import firebase from 'firebase/app'
-import { useCollectionData } from 'react-firebase-hooks/firestore';
 import './ChatMessage.scss'
 
 const ChatMessage = ({ message }) => {
